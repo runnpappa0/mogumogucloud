850be715559f67ac36e4861d2c4146c4a91ed772
lin <81099chan@gmail.com>
2024-12-15 19:20:05 +0900

    fix

--- templates/layouts/admin-footer.php
+++ templates/layouts/admin-footer.php
@@ -1,3 +1,3 @@
 <footer class="text-center py-3 bg-light">
-    <p class="mb-0">© 2024 Obento System. All rights reserved.</p>
+    <p class="mb-0">© 2024 Mogumogu Cloud. All rights reserved.</p>
 </footer>
