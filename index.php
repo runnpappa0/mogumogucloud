2aa13aefaefa4115f0f0334d908f85929901204b
lin <81099chan@gmail.com>
2024-12-17 19:10:08 +0900

    add favicon

--- index.php
+++ index.php
@@ -18,6 +18,7 @@ if (isset($_SESSION['user_id'])) {
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
     <link rel="stylesheet" href="/public/css/style.css">
+    <link rel="shortcut icon" href="./public/assets/favicon.png" type="image/x-icon">
 </head>
 
 <body>
